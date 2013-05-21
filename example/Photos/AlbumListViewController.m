52ceb7101d9474b6d06c4260f55645629026f0dc
Andrey Syvrachev <andreyalright@gmail.com>
2013-05-21 15:25:36 +0400

    MIT license

--- example/Photos/AlbumListViewController.m
+++ example/Photos/AlbumListViewController.m
@@ -9,7 +9,7 @@
 #import "AlbumListViewController.h"
 #import "ASAssetsLibrary.h"
 #import "GalleryViewController.h"
-#import "GalleryAsset.h"
+#import "ALAssetAdapter.h"
 
 @interface AlbumListViewController ()
 
@@ -79,44 +79,6 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
@@ -134,7 +96,7 @@
     [group enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
 
         if (result){
-            GalleryAsset* asset = [[GalleryAsset alloc] init];
+            ALAssetAdapter* asset = [[ALAssetAdapter alloc] init];
             asset.asset = result;
             
             [assets addObject:asset];
